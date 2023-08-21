a5c3b2d8a7212a93eb0d0d62933883d0da7a9017
Julian Ruiz Rojas <julrurojas@espol.edu.ec>
2023-08-21 11:07:21 -0500

    Cargar locales para mostrar en mapa y creacion de la clase Local

---
+++ PROYECTO 2P ENTREGABLE/ProyectoFinal/src/main/java/Clases/Local.java
@@ -0,0 +1,59 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Clases;
+
+/**
+ *
+ * @author Julian Ruiz
+ */
+public class Local {
+
+    public void setCoordenadax(double coordenadax) {
+        this.coordenadax = coordenadax;
+    }
+
+    public void setCoordenadaY(double coordenadaY) {
+        this.coordenadaY = coordenadaY;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setHorario(String horario) {
+        this.horario = horario;
+    }
+
+    public double getCoordenadax() {
+        return coordenadax;
+    }
+
+    public double getCoordenadaY() {
+        return coordenadaY;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getHorario() {
+        return horario;
+    }
+    double coordenadax;
+    double coordenadaY;
+    String nombre;
+    String horario;
+    
+    public Local(double cX,double cY,String n,String h){
+        this.coordenadax=cX;
+        this.coordenadaY=cY;
+        this.nombre=n;
+        this.horario=h;
+    }
+    
+    public String toString(){
+        return this.nombre+" "+this.horario;
+    }
+}
--- PROYECTO 2P ENTREGABLE/ProyectoFinal/src/main/java/espol/proyectofinal/LocalesController.java
+++ PROYECTO 2P ENTREGABLE/ProyectoFinal/src/main/java/espol/proyectofinal/LocalesController.java
@@ -4,10 +4,19 @@
  */
 package espol.proyectofinal;
 
+import Clases.Local;
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
 import javafx.scene.layout.Pane;
 /**
@@ -25,7 +34,49 @@ public class LocalesController implements Initializable {
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        mostrarLocales(cargarLocales());
     }    
     
+    public ArrayList<Local> cargarLocales(){
+        FileReader reader=null;
+        BufferedReader br=null;
+        ArrayList<Local> locales=new ArrayList<>();
+        try{
+            try{
+            reader=new FileReader(InicioVentana.pathFiles+"locales.txt");
+            }catch(FileNotFoundException e){
+                System.out.println("Archivo no encontrado");
+            }
+            br= new BufferedReader(reader);
+            String linea=br.readLine();
+            while(linea!=null){
+                String[] datos=linea.strip().split(",");
+                double cX=Double.parseDouble(datos[0]);
+                double cY=Double.parseDouble(datos[1]);
+                String nombre=datos[2];
+                String horario=datos[3];
+                Local l=new Local(cX,cY,nombre,horario);
+                linea=br.readLine();
+            }
+        }catch(IOException e){
+            System.out.println(e.getMessage());
+        }
+        return locales;
+    }
+    
+    public void mostrarLocales(ArrayList<Local> locales){
+        for(Local l:locales){
+            ImageView inv=null;
+            try{FileInputStream input=new FileInputStream(InicioVentana.pathPhotos+"helado.jpg");
+            Image image=new Image(input);
+            inv=new ImageView(image);
+        }catch(IOException e){
+                System.out.println(e.getMessage()); 
+        }
+            inv.setLayoutX(l.getCoordenadax());
+            inv.setLayoutY(l.getCoordenadaY());
+            rootlocales.getChildren().add(inv);
+        }
+    }
 }
+
